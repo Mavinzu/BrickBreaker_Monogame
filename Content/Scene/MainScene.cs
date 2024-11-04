36694c746580e2efcc35f693bd4145144df8d818
Mavinzu <Mavinzu@gmail.com>
2024-11-04 21:29:58 +0700

    Add base scene to the game

---
+++ Content/Scene/MainScene.cs
@@ -0,0 +1,20 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Scene;
+
+public static class MainScene
+{
+    public static void Init()
+    {
+
+    }
+    public static void Update(GameTime gameTime)
+    {
+        
+    }
+    public static void Draw(SpriteBatch spriteBatch)
+    {
+        
+    }
+}
